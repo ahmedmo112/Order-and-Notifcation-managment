ad7c93aef956bebcfb79caf3fd2af825743e63ce
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2023-12-29 21:02:09 +0200

    Implement classes in the order DB package.

--- src/main/java/com/main/Order/Database/OrderDB.java
+++ src/main/java/com/main/Order/Database/OrderDB.java
@@ -1,4 +1,14 @@
 package com.main.Order.Database;
 
+import com.main.Order.model.Order;
+
+import java.util.List;
+
 public interface OrderDB {
+    public List<Order> getOrders();
+
+    public Order getOrder(int orderId);
+    public void addOrder(Order order);
+
+    public void removeOrder(Order order);
 }
--- src/main/java/com/main/Order/Database/OrderInMemoryDB.java
+++ src/main/java/com/main/Order/Database/OrderInMemoryDB.java
@@ -1,7 +1,41 @@
 package com.main.Order.Database;
 
+import com.main.Order.model.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class OrderInMemoryDB {
+public class OrderInMemoryDB implements OrderDB {
+
+    private List<Order> orders;
+
+    OrderInMemoryDB() {
+        orders = new ArrayList<>();
+    }
+
+    @Override
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @Override
+    public Order getOrder(int orderId) {
+        for (Order order : orders) {
+            if (order.getOrderId() == orderId)
+                return order;
+        }
+        return null;
+    }
+
+    @Override
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    @Override
+    public void removeOrder(Order order) {
+        orders.remove(order);
+    }
 }
